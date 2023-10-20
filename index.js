8b1d572ee2e97a383a0cf9848cc2da9117e54eaf
Anthony <gachinigaithuma@gmail.com>
2023-10-20 07:23:27 +0300

    started challenge

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// document.addEventListener('DOMContentLoaded',function(){
+//     const review = document.getElementById('review');
+//     const submitBtn = document.getElementById('submitBtn');
+    
+
+//     submitBtn.addEventListener('click', addReview)
+// })
+// document.addEventListener('DOMContentLoaded', function(){
+//  document.getElementById('review-form').addEventListener('submit', function(event){
+//     event.preventDefault();
+
+//     var textValue = document.getElementById('review').value;
+//     console.log("The review:", textValue);
+//     addReview(textValue);
+// })
+// });
+// function addReview(review){
+//     var ul = document.getElementById('review-list');
+//     var li = document.createElement('li');
+//     li.appendChild(document.createTextNode(review));
+//     li.setAttribute('id', review);
+//     ul.appendChild(li);
+
+// }
